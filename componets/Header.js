e1f29c7ac5d985034ff74aea89e288c03a5e3909
Chaoyi Shi <121272886+ChaoyiShi98033@users.noreply.github.com>
2024-01-20 13:13:40 -0800

    add text and text input

--- componets/Header.js
+++ componets/Header.js
@@ -2,10 +2,11 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function Header(props) {
-    console.log(props);
+    //console.log(props);
   return (
     <View>
-      <Text>Welcome to {props.name} {props.version} </Text>
+      <Text>Welcome to {props.name}</Text>
+      {props.children}
     </View>
   )
 }
