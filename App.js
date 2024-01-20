57357fd36aa178618894e3e12d5607ee7d23caba
Chaoyi Shi <121272886+ChaoyiShi98033@users.noreply.github.com>
2024-01-19 16:00:13 -0800

    added a Header

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './componets/Header';
 
 export default function App() {
   const appName = "My Awesome App";
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
+      <Header name = "My Awesome app" version = {2} />
     </View>
   );
 }
