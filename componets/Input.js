16a6c4a32ee0e330de3ff30c593601ee06391b53
Chaoyi Shi <121272886+ChaoyiShi98033@users.noreply.github.com>
2024-01-30 12:32:49 -0800

    styles and modal

--- componets/Input.js
+++ componets/Input.js
@@ -1,38 +1,75 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import { useState } from 'react'
+import {
+  Button,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 
+export default function Input({ inputHandler, modalVisible, dismissModal }) {
+  const [text, setText] = useState("");
+  // callback handler
+  function changeTextHandler(changedText) {
+    console.log("user is typing ", changedText);
 
-export default function input({inputHandler}) {
-    console.log(inputHandler);
-    const [text, setText] = useState("");
+    setText(changedText);
+  }
 
-    function changeTextHandler (changedText) {
-        console.log("using is typing", changedText);
-        setText(changedText);
-      }
-
-    function confirmHandler() {
-        inputHandler(text);
-    }
-
+  function confirmHandler() {
+    inputHandler(text);
+  }
+  function cancelHandler() {
+    // hide the modal
+    dismissModal();
+  }
   return (
-    <View>
-      <TextInput 
-      placeholder = "Type something"
-      style={styles.input} 
-      value={text} 
-      onChangeText={changeTextHandler} 
-      />
-      <Button title="confirm" onPress={function(){inputHandler(text)}}/>
-    </View>
-  )
+    <Modal visible={modalVisible}>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+          }}
+          style={styles.image}
+        />
+        <Image source={require("../assets/favicon.png")} style={styles.image} />
+        <TextInput
+          placeholder="Type something"
+          style={styles.input}
+          value={text}
+          onChangeText={changeTextHandler}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonView}>
+            <Button title="Cancel" onPress={cancelHandler} />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="Confirm" onPress={confirmHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    input: {
-        borderBottomWidth: 2,
-        borderBottomColor: "red",
-        width: "50%",
-      }
-})
+  buttonView: {
+    width: "30%",
+    margin: 5,
+  },
+  buttonsContainer: { flexDirection: "row" },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "purple",
+    width: "50%",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: { width: 100, height: 100 },
+});
